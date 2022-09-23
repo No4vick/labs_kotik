50850705a3c8cee86be6874753373c14f0796912
No4vick <75495000+No4vick@users.noreply.github.com>
2022-09-24 00:37:43 +0300

    git commit nice

--- Coder.py
+++ Coder.py
@@ -1,79 +1,59 @@
 import os
+import sys
 
-def get_header(header_path, version: int = 1, sub_version: int = 1):#, ctx_coding: int = 0, ctxless_coding: int = 0, cypher: int = 0):
-    with open(header_path + ".tmp", 'w+b') as f:
-        f.write(bytes("BRIGADE7", encoding='ascii'))
-        f.write(version.to_bytes(2, byteorder='big', signed=False))
-        f.write(sub_version.to_bytes(2, byteorder='big', signed=False))
-        header = f.read()
-    os.remove(header_path + ".tmp")
+
+def get_header(version: int = 1, sub_version: int = 1):#, ctx: int = 0, no_ctx: int = 0, cypher: int = 0):
+    header = bytes("BRIGADE7", encoding='ascii') + version.to_bytes(2, byteorder='big', signed=False) + \
+             sub_version.to_bytes(2, byteorder='big', signed=False)
     return header
 
 
-
-def coder(header_name, name):
+def coder(name, src_folder):
     # создадим переменные под исходный и конечный
     # размеры архива
-    original_size = os.path.getsize(f"{header_name}")
+    header_size = 64
+    original_size = 64
     final_size = 0
 
     # создание нового файла и вставка в него заголовка
-    new_file = open(name, "wb")
-    header = get_header(header_name, version=1, sub_version=0)
+    try:
+        new_file = open(name, "wb")
+    except OSError as e:
+        print(str(e) + "\n" + "Couldn't open file")
+        return
+    header = get_header(version=1, sub_version=0)
     new_file.write(header)
+    new_file.seek(header_size)
 
-    for root, dirs, files in os.walk("files"):
-        # один байт будет выделен чтобы отличать папку от файла
-        # 0 для папки
-        # 1 для файла
-
-        # запись папок в архив
-        for dirname in dirs:
-            new_file.write(int(0).to_bytes(1, 'big'))
-            p = os.path.join(root, dirname)
-            p = p.split('\\')
-            p = p[-2] + '\\' + p[-1]
-            pathsize = len(p)
-            new_file.write(pathsize.to_bytes(4, byteorder='big', signed=False))
-            new_file.write(bytes(p, encoding='utf-8'))
-            original_size += pathsize
-
+    for root, dirs, files in os.walk(src_folder):
         # вставка файлов в архив
         for filename in files:
-
-            new_file.write(int(1).to_bytes(1, 'big'))
+            # new_file.write(True.to_bytes(1, 'big'))
+            new_file.write(bytes("B7", encoding='ascii'))
             # находим в каких каталогах находятся файлы
             # записываем в 'p' в виде folder\filename
-
-            p = os.path.join(root, filename)
-            # print(p)
+            p = os.path.join(root, filename).replace('\\', '/')
             # при архивировании будем сначала выделять 8 байтов под размер самого файла
             # и 4 байта под размер имени файла + папки
             # затем со смещением 12 байт будет размещаться уже сам файл
             with open(p, 'rb') as f:
                 filesize = os.path.getsize(p)
                 new_file.write(filesize.to_bytes(8, byteorder='big', signed=False))
-                p = p.split('\\')
-                p = p[-2] + '\\' + p[-1]
-                pathsize = len(p)
+                # перекодирование строки в utf-8
+                pathsize = len(p.encode(encoding='utf-8'))
                 new_file.write(pathsize.to_bytes(4, byteorder='big', signed=False))
                 original_size += filesize + pathsize
-
-                # print(p)
                 new_file.write(bytes(p, encoding='utf-8'))
                 new_file.write(f.read())
 
-    # print(original_size)
-    # print(original_size.to_bytes(8, byteorder='big', signed=False))
     # запишем размеры архива в header
     # исходный
     new_file.seek(12, 0)
     new_file.write(original_size.to_bytes(8, byteorder='big', signed=False))
     # конечный
-    # new_file.seek(20, 0)
     new_file.write(original_size.to_bytes(8, byteorder='big', signed=False))
+    new_file.close()
 
 
 if __name__ == '__main__':
-    coder('header0', 'archive')
-    # get_header('header0')
+    coder('archive', 'files')
--- Decoder.py
+++ Decoder.py
@@ -1,39 +1,44 @@
 import os
 
 
-def decoder(header_name, archive_name):
+def decoder(archive_name):
     signature = ''
-    header_size = os.path.getsize(header_name)
+    header_size = 64
 
-    #считаем сигнатуру и размер header
-    with open(header_name, 'rb') as header:
-        signature = header.read(8).decode(encoding='utf-8')
+    # считаем сигнатуру и размер header
+    signature = "BRIGADE7"
 
-    print(signature)
-    #разбираем header в архиве
+    # разбираем header в архиве
     with open(archive_name, 'rb') as archive:
-        #проверка сигнатуры
+        # проверка сигнатуры
         if signature == archive.read(8).decode(encoding='utf-8'):
-        #проверка кодов алгоритмов
+            # проверка кодов алгоритмов
             archive.seek(28, 0)
             with_context = int.from_bytes(archive.read(1), 'big')
             without_context = int.from_bytes(archive.read(1), 'big')
-            if with_context == without_context == 0:
-                # os.mkdir('files_unpacked')
-                archive.seek(64, 0)
-                isfile = int.from_bytes(archive.read(1), 'big')
-                if isfile == 1:
-                    pass
-                if isfile == 0:
-                    namesize = int.from_bytes(archive.read(4), 'big')
-                    folder, name = archive.read(namesize).decode('utf-8').split('\\')
-                    print(name)
-                    # os.mkdir('')
-                    pass
+            archive.seek(64)
+            if archive.read(2) != b'B7':
+                print('No files found')
+                return
+            while True:
+                # Считывание размера файла и размера пути
+                size = int.from_bytes(archive.read(8), byteorder='big', signed=False)
+                path_size = int.from_bytes(archive.read(4), byteorder='big', signed=False)
+                # Считывание и декодирование пути
+                path = os.path.normpath(archive.read(path_size).decode(encoding='utf-8')).replace('\\', '/')
+                file = archive.read(size)
+                # Создание пути папок
+                folder_path = path[:path.rfind('/')]
+                os.makedirs(folder_path, exist_ok=True)
+                # Запись файла
+                with open(path, 'wb') as f:
+                    f.write(file)
+                # Если не нашлась следующая сигнатура выходим
+                if archive.read(2) != b'B7':
+                    break
         else:
-            print('неверный формат файла')
-
+            print('Неверный формат файла')
 
 
 if __name__ == '__main__':
-    decoder('header0', 'archive')
+    decoder('archive')
